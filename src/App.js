5317284797104e20435c2585b58296da427b3ebb
Cory Fladhammer <cjfladham@hotmail.com>
2020-04-17 08:14:31 -0500

    correct folder display error

--- src/App.js
+++ src/App.js
@@ -6,161 +6,91 @@ import AddFolder from './AddFolder'
 import ShowNotesForFolder from './ShowNotesForFolder'
 import ShowOneNote from './ShowOneNote'
 import AddNote from './AddNote'
-import dummyStore from './dummy-store'
 import { findNote, getNotesForFolder } from './notes-helpers'
 import config from './config';
 import NotefulContext from './NotefulContext';
 
 
 class App extends React.Component {
-  state = {
-    notes: [],
-    folders: []
-  }
+    state = {
+        notes: [],
+        folders: []
+    };
 
-  componentDidMount() {
-    // setTimeout(() => this.setState(dummyStore), 500)
-    // console.log(dummyStore)
+    componentDidMount() {
+        Promise.all([
+            fetch(`${config.API_ENDPOINT}/notes`),
+            fetch(`${config.API_ENDPOINT}/folders`)
+        ])
+            .then(([notesRes, foldersRes]) => {
+                if (!notesRes.ok)
+                    return notesRes.json().then(e => Promise.reject(e));
+                if (!foldersRes.ok)
+                    return foldersRes.json().then(e => Promise.reject(e));
 
-    Promise.all([
-      fetch(`${config.API_ENDPOINT}/notes`),
-      fetch(`${config.API_ENDPOINT}/folders`)
-    ])
-      .then(([notesRes, foldersRes]) => {
-        if (!notesRes.ok)
-          return notesRes.json().then(e => Promise.reject(e))
-        if (!foldersRes.ok)
-          return foldersRes.json().then(e => Promise.reject(e))
-
-        return Promise.all([notesRes.json(), foldersRes.json()])
-      })
-      .then(([notes, folders]) => {
-        // console.log(notes, folders)
-        this.setState({ notes, folders })
-        console.log(this.state)
-      })
-      .catch(error => {
-        console.error({ error })
-      })
-  }
-
-  addFolder = folder => {
-    this.setState({
-      folders: [...this.state.folders, folder]
-    })
-  }
-
-  // getFolders = () => {
-  //   fetch(config.API_FOLDERS, {
-  //     method: 'GET',
-  //     headers: {
-  //       'content-type': 'application/json'
-  //     }
-  //   })
-  //   .then(res => {
-  //     if (!res.ok) {
-  //       throw new Error(res.status)
-  //     }
-  //     return res.json()
-  //   })
-  //   .then((response) => {
-  //     console.log(response)
-  //     // this.setState({ notes, folders })
-  //     // console.log(this.state)
-  //   })
-  //   .then(this.setFolders)
-  //   .catch(error => this.setState({ foldersError: error}))
-  // }
- 
-
-  renderNavRoutes() {
-    const { notes, folders } = this.state;
-    return (
-      <>
-        {['/', '/folder/:folderId'].map(path => (
-          <Route
-            exact
-            key={path}
-            path={path}
-            render={routeProps => (
-              <ShowFolders
-                folders={folders}
-                notes={notes}
-                {...routeProps}
-              />
-            )}
-          />
-        ))}
-        <Route path="/note/:noteId" />
-        <Route path="/add-folder" component={AddFolder} />
-        <Route path="/add-note" component={AddNote} />
-      </>
-    );
-  }
-
-  renderMainRoutes() {
-    const { notes } = this.state;
-    return (
-      <>
-        {['/', '/folder/:folderId'].map(path => (
-          <Route
-            exact
-            key={path}
-            path={path}
-            render={routeProps => {
-              const { folderId } = routeProps.match.params;
-              const notesForFolder = getNotesForFolder(
-                notes,
-                folderId
-              );
-              return (
-                <ShowNotesForFolder
-                  {...routeProps}
-                  notes={notesForFolder}
-                />
-              );
-            }}
-          />
-        ))}
-        <Route
-          path="/note/:noteId"
-          render={routeProps => {
-            const { noteId } = routeProps.match.params;
-            const note = findNote(notes, noteId);
-            return <ShowOneNote {...routeProps} note={note} />;
-          }}
-        />
-      </>
-    );
-  }
-
-  render() {
-    const contextValue = {
-      folders: this.state.folders,
-      notes: this.state.notes,
-      addFolder: this.addFolder,
-
+                return Promise.all([notesRes.json(), foldersRes.json()]);
+            })
+            .then(([notes, folders]) => {
+                this.setState({notes, folders});
+            })
+            .catch(error => {
+                console.error({error});
+            });
     }
-    return (
-      <NotefulContext.Provider value={contextValue}>
-      <div className='app'>
-        <nav>{this.renderNavRoutes()}</nav>
-        <header>
-          <h1>
-            <Link to='/'>Noteful</Link>{' '}
-          </h1>
-        </header>
-        <main>{this.renderMainRoutes()}</main>
 
-      </div>
-      </NotefulContext.Provider>
+    renderNavRoutes() {
+        return (
+            <>
+                {['/', '/folder/:folderId'].map(path => (
+                    <Route
+                        exact
+                        key={path}
+                        path={path}
+                        component={ShowFolders}
+                    />
+                ))}
+                <Route path="/note/:noteId"  />
+                <Route path="/add-folder" component={AddFolder} />
+                <Route path="/add-note" component={AddNote} />
+            </>
+        );
+    }
 
-    )
-  }
+    renderMainRoutes() {
+        return (
+            <>
+                {['/', '/folder/:folderId'].map(path => (
+                    <Route
+                        exact
+                        key={path}
+                        path={path}
+                        component={ShowNotesForFolder}
+                    />
+                ))}
+                <Route path="/note/:noteId" component={ShowOneNote} />
+            </>
+        );
+    }
+
+    render() {
+        const value = {
+            notes: this.state.notes,
+            folders: this.state.folders,
+        };
+        return (
+            <NotefulContext.Provider value={value}>
+                <div className="App">
+                    <nav>{this.renderNavRoutes()}</nav>
+                    <header >
+                        <h1>
+                            <Link to="/">Noteful</Link>{' '}
+                        </h1>
+                    </header>
+                    <main>{this.renderMainRoutes()}</main>
+                </div>
+            </NotefulContext.Provider>
+        );
+    }
 }
 
 export default App;
-
-ShowFolders.defaultProps = {
-  folders: []
-}
--- src/ShowFolders.js
+++ src/ShowFolders.js
@@ -6,51 +6,70 @@ import NotefulContext from './NotefulContext'
 
 class ShowFolders extends React.Component {
 
-    static contextType = NotefulContext
+  static contextType = NotefulContext
 
-    render() {
-        const { folders = [] } = this.context
-        console.log(this.context)
-        const folderId = folders.map(folder => folder.id)
-        
-        const findFolder = (folders = [], folderId) =>
-            {
-                console.log(folders, folderId)
-                folders.find(folder => folder.id == folderId)
-            }
-            
+  render() {
 
-        const folderDisplay = findFolder(folders, folderId);
-        console.log(folderDisplay)
-        return (
-            <div className='showFolders'>
-                <ul>
-                    <p>SHOW FOLDERS</p>
-                    <p>Folders: </p>
-                    {this.props.folders.map(folder =>
-                        <li key={folder.id}>
-                            <NavLink
-                                to={`/folder/${folder.id}`}
-                            >
-                                <span>
-                                    {countNotesForFolder(this.props.notes, folder.id)}
-                                </span>
-                                {folder.name}
-                            </NavLink>
-                            {folderDisplay}.....
-                        </li>)}
-                </ul>
-                <div>
-                    <CircleButton
-                        tag={Link}
-                        to='/add-folder'
-                    >
-                        Add Folder
-                    </CircleButton>
-                </div>
-            </div>
-        )
-    }
+    const { folders = [], notes = [] } = this.context
+
+    console.log(this.context)
+
+    return (
+      <div >
+        <ul >
+          {folders.map(folder =>
+            <li key={folder.id}>
+              <NavLink
+                to={`/folder/${folder.id}`}
+              >
+                {countNotesForFolder(notes, folder.id)}
+              
+                {folder.folder_name}
+              </NavLink>
+            </li>
+          )}
+        </ul>
+        <div>
+          <CircleButton
+            tag={Link}
+            to='/add-folder'
+            type='button'
+          >
+            <br />
+                  Add Folder
+                </CircleButton>
+        </div>
+      </div>
+    )
+    // return (
+    //     <div className='showFolders'>
+    //         <ul>
+    //             <p>SHOW FOLDERS</p>
+    //             <p>Folders: </p>
+    //             {this.props.folders.map(folder =>
+    //                 <li key={folder.id}>
+    //                     <NavLink
+    //                         to={`/folder/${folder.id}`}
+    //                     >
+    //                         <span>
+    //                             {countNotesForFolder(this.props.notes, folder.id)}
+    //                         </span>
+    //                         {folder.name}
+    //                     </NavLink>
+    //                     {/* {folderDisplay}..... */}
+    //                 </li>)}
+    //         </ul>
+    //         <div>
+    //             <CircleButton
+    //                 tag={Link}
+    //                 to='/add-folder'
+    //             >
+    //                 Add Folder
+    //             </CircleButton>
+    //         </div>
+    //     </div>
+    // )
+  }
 
 }
 
